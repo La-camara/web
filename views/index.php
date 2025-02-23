2c49e2a5d93a1541fedf6a0e185f068449091192
robin <robincoello@hotmail.com>
2025-02-23 16:22:34 +0100

    add files

---
+++ views/index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+        <title>eNno Bootstrap Template - Index</title>
+        <meta content="" name="description">
+        <meta content="" name="keywords">
+
+        <!-- Favicons -->
+        <link href="www/public_html/eNno/views/assets/img/favicon.png" rel="icon">
+        <link href="www/public_html/eNno/views/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+        <!-- Vendor CSS Files -->
+        <link href="www/public_html/eNno/views/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="www/public_html/eNno/views/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+        <link href="www/public_html/eNno/views/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+        <link href="www/public_html/eNno/views/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+        <link href="www/public_html/eNno/views/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+        <!-- Template Main CSS File -->
+        <link href="www/public_html/eNno/views/assets/css/style.css" rel="stylesheet">
+
+        <?php
+        /**
+         * 
+          <!-- =======================================================
+         * Template Name: eNno - v4.7.0
+         * Template URL: https://bootstrapmade.com/enno-free-simple-bootstrap-template/
+         * Author: BootstrapMade.com
+         * License: https://bootstrapmade.com/license/
+          ======================================================== -->
+         */
+        ?>
+    </head>
+
+    <body>
+
+        <header id="header" class="fixed-top">
+            <div class="container d-flex align-items-center justify-content-between">
+
+                <h1 class="logo"><a href="index.html">La camara</a></h1>
+
+                <!-- Uncomment below if you prefer to use an image logo -->
+                <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+                <?php include "nav.php"; ?>
+
+            </div>
+        </header>
+        
+        <?php include "hero.php"; ?>
+
+        <main id="main">
+
+            <?php include "featured-services.php"; ?>
+            <?php include "about.php"; ?>
+            <?php include "counts.php"; ?>
+            <?php include "services.php"; ?>
+            <?php include "portfolio.php"; ?>
+            <?php include "testimonials.php"; ?>
+            <?php include "cta.php"; ?>
+            <?php include "team.php"; ?>
+            <?php include "contact.php"; ?>
+            
+
+        </main>
+        
+        <?php include "footer.php"; ?>
+
+        <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+        <!-- Vendor JS Files -->
+        <script src="www/public_html/eNno/views/assets/vendor/purecounter/purecounter.js"></script>
+        <script src="www/public_html/eNno/views/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="www/public_html/eNno/views/assets/vendor/glightbox/js/glightbox.min.js"></script>
+        <script src="www/public_html/eNno/views/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+        <script src="www/public_html/eNno/views/assets/vendor/swiper/swiper-bundle.min.js"></script>
+        <script src="www/public_html/eNno/views/assets/vendor/php-email-form/validate.js"></script>
+
+        <!-- Template Main JS File -->
+        <script src="www/public_html/eNno/views/assets/js/main.js"></script>
+
+    </body>
+
+</html>
