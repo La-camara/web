506228a826153518d1f30efac701450fa0e3b42b
robin <robincoello@hotmail.com>
2025-02-23 16:28:34 +0100

    mep

--- copy.sh
+++ copy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Copia de factuz a el repo aparte"
 
-mv /var/www/html/factuz_61/www/public_html/eNno/* /var/www/html/web/
+cp -R /var/www/html/factuz_61/www/public_html/eNno/* /var/www/html/web/
